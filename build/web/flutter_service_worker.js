3bf527f799a1ef836a47c375866138b33d73e992
UttamkiniH <huttamkini23@gmail.com>
2021-08-29 03:44:53 +0530

    Deployed

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,52 +3,56 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "8365420078db6ff55648f3cbfdb98aea",
-"/": "8365420078db6ff55648f3cbfdb98aea",
-"main.dart.js": "f60ed0d8cbae93b7694c147188ddbdfc",
+  "assets/AssetManifest.json": "46f5ab28daeb2b370df5ccb2d5845ca7",
+"assets/assets/acm.png": "f1a0380d4897fad98e89cf0f5846b10d",
+"assets/assets/an96.png": "a97e8355bcdad5d96835754bd1c1228b",
+"assets/assets/android.png": "7a2406d990f87c34fa3246142d6c390f",
+"assets/assets/bRprofile.png": "044c7a62f42852cda7826e55ce330c28",
+"assets/assets/C++-programming.png": "a15ba0c670a57fd09076b7cfde43615e",
+"assets/assets/c-programming.png": "e1b4170830bba92c656a06913ab5a888",
+"assets/assets/cloud-engine.png": "b76e3a5e9cd9d2c30164ff9a87d9f8c2",
+"assets/assets/flutter.png": "fa72db9cba7c71dc4f26396b71bba35a",
+"assets/assets/gdg.png": "3ceca25672bd98043466c27645e9645d",
+"assets/assets/gdgd.png": "b1531c211e907039085591ee4ce8367d",
+"assets/assets/gh.png": "36e8af7ef6443d63d239cb6831767758",
+"assets/assets/gihub-color.png": "f8765d646d4ea7e3c4401d2eddb7e115",
+"assets/assets/git.png": "7fa2550a0027e20604161bf289d5850f",
+"assets/assets/github.png": "7303fcf0bbe2ecccc0877ff2e6ce7a28",
+"assets/assets/logo.png": "f603287744d224cfd19c57e1f158fec2",
+"assets/assets/pic.png": "d74ba47b0ea0b4b2b4ebb1d3e334e769",
+"assets/assets/pic2.png": "e7c81ab00ae740a94702b008502aef9e",
+"assets/assets/qwiklabs_logo.png": "b00a5bdc899d00fc4c856d9977c89102",
+"assets/assets/sosc.png": "e81da50afdaa27416c7b9d9676e73c74",
+"assets/assets/visual-studio.png": "925bfbb2ed235afcb240e8f06cfbfa7b",
+"assets/FontManifest.json": "0fbcce73eec6d7173ace6d9459d86614",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/NOTICES": "bcd6250dddc0407c8a2a046d35fb8857",
+"assets/packages/ant_icons/lib/icons/ant.ttf": "ea823ee1ad8c6c7b1b83dfe7d074bed4",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b14fcf3ee94e3ace300b192e9e7c8c5d",
+"assets/packages/flutter_icons/fonts/AntDesign.ttf": "3a2ba31570920eeb9b1d217cabe58315",
+"assets/packages/flutter_icons/fonts/Entypo.ttf": "744ce60078c17d86006dd0edabcd59a7",
+"assets/packages/flutter_icons/fonts/EvilIcons.ttf": "140c53a7643ea949007aa9a282153849",
+"assets/packages/flutter_icons/fonts/Feather.ttf": "6beba7e6834963f7f171d3bdd075c915",
+"assets/packages/flutter_icons/fonts/FontAwesome.ttf": "b06871f281fee6b241d60582ae9369b9",
+"assets/packages/flutter_icons/fonts/FontAwesome5_Brands.ttf": "c39278f7abfc798a241551194f55e29f",
+"assets/packages/flutter_icons/fonts/FontAwesome5_Regular.ttf": "f6c6f6c8cb7784254ad00056f6fbd74e",
+"assets/packages/flutter_icons/fonts/FontAwesome5_Solid.ttf": "b70cea0339374107969eb53e5b1f603f",
+"assets/packages/flutter_icons/fonts/Foundation.ttf": "e20945d7c929279ef7a6f1db184a4470",
+"assets/packages/flutter_icons/fonts/Ionicons.ttf": "b2e0fc821c6886fb3940f85a3320003e",
+"assets/packages/flutter_icons/fonts/MaterialCommunityIcons.ttf": "3c851d60ad5ef3f2fe43ebd263490d78",
+"assets/packages/flutter_icons/fonts/MaterialIcons.ttf": "a37b0c01c0baf1888ca812cc0508f6e2",
+"assets/packages/flutter_icons/fonts/Octicons.ttf": "73b8cff012825060b308d2162f31dbb2",
+"assets/packages/flutter_icons/fonts/SimpleLineIcons.ttf": "d2285965fe34b05465047401b8595dd0",
+"assets/packages/flutter_icons/fonts/weathericons.ttf": "4618f0de2a818e7ad3fe880e0b74d04a",
+"assets/packages/flutter_icons/fonts/Zocial.ttf": "5cdf883b18a5651a29a4d1ef276d2457",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"manifest.json": "a83bbbd9d23ea4d752fbcd44cdc544fa",
-"assets/AssetManifest.json": "23eab71089fb6b79e9663a9642e39cce",
-"assets/NOTICES": "ba03f8c2b7901bf05693212efb05f818",
-"assets/FontManifest.json": "0fbcce73eec6d7173ace6d9459d86614",
-"assets/packages/ant_icons/lib/icons/ant.ttf": "ea823ee1ad8c6c7b1b83dfe7d074bed4",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b14fcf3ee94e3ace300b192e9e7c8c5d",
-"assets/packages/flutter_icons/fonts/Octicons.ttf": "73b8cff012825060b308d2162f31dbb2",
-"assets/packages/flutter_icons/fonts/Feather.ttf": "6beba7e6834963f7f171d3bdd075c915",
-"assets/packages/flutter_icons/fonts/Entypo.ttf": "744ce60078c17d86006dd0edabcd59a7",
-"assets/packages/flutter_icons/fonts/FontAwesome5_Brands.ttf": "c39278f7abfc798a241551194f55e29f",
-"assets/packages/flutter_icons/fonts/MaterialCommunityIcons.ttf": "3c851d60ad5ef3f2fe43ebd263490d78",
-"assets/packages/flutter_icons/fonts/AntDesign.ttf": "3a2ba31570920eeb9b1d217cabe58315",
-"assets/packages/flutter_icons/fonts/Foundation.ttf": "e20945d7c929279ef7a6f1db184a4470",
-"assets/packages/flutter_icons/fonts/weathericons.ttf": "4618f0de2a818e7ad3fe880e0b74d04a",
-"assets/packages/flutter_icons/fonts/Ionicons.ttf": "b2e0fc821c6886fb3940f85a3320003e",
-"assets/packages/flutter_icons/fonts/FontAwesome5_Solid.ttf": "b70cea0339374107969eb53e5b1f603f",
-"assets/packages/flutter_icons/fonts/FontAwesome5_Regular.ttf": "f6c6f6c8cb7784254ad00056f6fbd74e",
-"assets/packages/flutter_icons/fonts/FontAwesome.ttf": "b06871f281fee6b241d60582ae9369b9",
-"assets/packages/flutter_icons/fonts/Zocial.ttf": "5cdf883b18a5651a29a4d1ef276d2457",
-"assets/packages/flutter_icons/fonts/EvilIcons.ttf": "140c53a7643ea949007aa9a282153849",
-"assets/packages/flutter_icons/fonts/SimpleLineIcons.ttf": "d2285965fe34b05465047401b8595dd0",
-"assets/packages/flutter_icons/fonts/MaterialIcons.ttf": "a37b0c01c0baf1888ca812cc0508f6e2",
-"assets/fonts/MaterialIcons-Regular.otf": "a68d2a28c526b3b070aefca4bac93d25",
-"assets/assets/visual-studio.png": "925bfbb2ed235afcb240e8f06cfbfa7b",
-"assets/assets/gdgd.png": "b1531c211e907039085591ee4ce8367d",
-"assets/assets/an96.png": "a97e8355bcdad5d96835754bd1c1228b",
-"assets/assets/git.png": "7fa2550a0027e20604161bf289d5850f",
-"assets/assets/c-programming.png": "e1b4170830bba92c656a06913ab5a888",
-"assets/assets/gdg.png": "3ceca25672bd98043466c27645e9645d",
-"assets/assets/flutter.png": "fa72db9cba7c71dc4f26396b71bba35a",
-"assets/assets/github.png": "7303fcf0bbe2ecccc0877ff2e6ce7a28",
-"assets/assets/android.png": "7a2406d990f87c34fa3246142d6c390f",
-"assets/assets/sosc.png": "e81da50afdaa27416c7b9d9676e73c74",
-"assets/assets/pic2.png": "e7c81ab00ae740a94702b008502aef9e",
-"assets/assets/logo.png": "f603287744d224cfd19c57e1f158fec2",
-"assets/assets/ACM.png": "f1a0380d4897fad98e89cf0f5846b10d",
-"assets/assets/cloud-engine.png": "b76e3a5e9cd9d2c30164ff9a87d9f8c2",
-"assets/assets/gihub-color.png": "f8765d646d4ea7e3c4401d2eddb7e115",
-"assets/assets/pic.png": "d74ba47b0ea0b4b2b4ebb1d3e334e769",
-"assets/assets/C++-programming.png": "a15ba0c670a57fd09076b7cfde43615e"
+"index.html": "52e8036d06b8891bc3eb9425c0f0d0b4",
+"/": "52e8036d06b8891bc3eb9425c0f0d0b4",
+"main.dart.js": "c1601cf44e5986dabda8924d80f23685",
+"manifest.json": "5a2c0526f1fe73e6f211bc7a9d15c210",
+"version.json": "426313f2f3133c2f20415344c4a22df3"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -62,10 +66,11 @@ const CORE = [
 "assets/FontManifest.json"];
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
@@ -130,6 +135,9 @@ self.addEventListener("activate", function(event) {
 // The fetch handler redirects requests for RESOURCE files to the service
 // worker cache.
 self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
   var origin = self.location.origin;
   var key = event.request.url.substring(origin.length + 1);
   // Redirect URLs to the index.html
@@ -139,9 +147,10 @@ self.addEventListener("fetch", (event) => {
   if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
     key = '/';
   }
-  // If the URL is not the RESOURCE list, skip the cache.
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
   if (!RESOURCES[key]) {
-    return event.respondWith(fetch(event.request));
+    return;
   }
   // If the URL is the index.html, perform an online-first request.
   if (key == '/') {
@@ -165,10 +174,12 @@ self.addEventListener('message', (event) => {
   // SkipWaiting can be used to immediately activate a waiting service worker.
   // This will also require a page refresh triggered by the main worker.
   if (event.data === 'skipWaiting') {
-    return self.skipWaiting();
+    self.skipWaiting();
+    return;
   }
-  if (event.message === 'downloadOffline') {
+  if (event.data === 'downloadOffline') {
     downloadOffline();
+    return;
   }
 });
 
@@ -185,7 +196,7 @@ async function downloadOffline() {
     }
     currentContent[key] = true;
   }
-  for (var resourceKey in Object.keys(RESOURCES)) {
+  for (var resourceKey of Object.keys(RESOURCES)) {
     if (!currentContent[resourceKey]) {
       resources.push(resourceKey);
     }
